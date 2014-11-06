65872dd763379c903bc36e03954115127e5e086c
hophop408 <Christopher.Shell@comcast.net>
2014-11-05 17:55:55 -0700

    Update RobotMain.java

--- src/edu/wpi/first/wpilibj/usfirst/FRC4068/RobotMain.java
+++ src/edu/wpi/first/wpilibj/usfirst/FRC4068/RobotMain.java
@@ -71,38 +71,8 @@ public class RobotMain extends SimpleRobot {
             
             /**
              * Used for the drive train movement.
-             * Should be less jerky.
-             * Feel free to modify where necessary.
-             * Should work for most drive train designs.
              */
-if (driverjoystick>=[8]){
-motor power = 100%;
-}
-else if (driverjoystick<=[7]){
-motor power = 87.5%;
-
-if (driverjoystick<=[6]){
-motor power = 75%;
-}
-else if (driverjoystick<=[5]){
-motor power = 62.5%;
-
-if (driverjoystick<=[4]){
-motor power = 50%;
-}
-else if (driverjoystick<=[3]){
-motor power = 37.5%;
-
-if (driverjoystick<=[2]){
-motor power = 25%;
-}
-else if (driverjoystick<=[1]){
-motor power = 12.5%;
-}
-}
-}
-}
-
+            motor power = driverjoystick^2;
 
              
         }
